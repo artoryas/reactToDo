e3b309587187b074e4eabd917e071e3f3fc2d9aa
Yerniyaz Dossanov <dosanov.erniyaz@gmail.com>
2019-11-06 15:26:13 +0600

    Updates

---
+++ precache-manifest.263a9ec3e3f7c3e27150e9238f74d9fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e8088dd72579be6292c2bfba1e10d54",
+    "url": "/reactToDo/index.html"
+  },
+  {
+    "revision": "79453ea6ba7ee7326452",
+    "url": "/reactToDo/static/css/main.e193f3c5.chunk.css"
+  },
+  {
+    "revision": "9eac536c9bd2e212ae97",
+    "url": "/reactToDo/static/js/2.c23d7400.chunk.js"
+  },
+  {
+    "revision": "79453ea6ba7ee7326452",
+    "url": "/reactToDo/static/js/main.28976c2d.chunk.js"
+  },
+  {
+    "revision": "86fa623aefc18451cae6",
+    "url": "/reactToDo/static/js/runtime-main.36f96e70.js"
+  }
+]);
